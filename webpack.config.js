15625162f952f92f6d1fd0c510c0867a39ae3722
acsolis896 <104403741+acsolis896@users.noreply.github.com>
2023-03-16 15:47:30 -0400

    Deleted presets from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,5 @@ module.exports = {
   "entry": {
     "index": "./index"
   },
-
-  "presets": [
-    "babel-preset-env",
-    "babel-preset-react"
-    ],
- "plugins": ["babel-plugin-transform-object-rest-spread"]
+  
 };
